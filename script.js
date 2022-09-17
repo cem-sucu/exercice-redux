f27ce67ff22e0702deee18b24e9ea74f100ae574
cem-sucu <cems.cems.17@gmail.com>
2022-09-17 10:19:36 +0200

    reducer dans redux

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//action
 const BUY_PHONE = "BUY_PHONE";
 
 function buyPhone() {
@@ -5,3 +6,23 @@ function buyPhone() {
         type: "buyPhone",
     };
 }
+
+//reducer
+//(prevState, action ) => newState
+
+const initialState = {
+    phones: 5,
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case BUY_PHONE:
+            return {
+                ...state, // ce ...state dans ce cas dz figue n'est pas utile car il n'ya qu'une propriété phone, mais s'il y avait plusuieur propriété elle serais utiles..
+                phones: state.phones - 1,
+            };
+
+        default:
+            return state;
+    }
+};
